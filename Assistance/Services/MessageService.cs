90f73ee7d254a3cfc9be203ba06bee07f6a915f7
fjair <3518110911@uth.edu.mx>
2023-09-06 17:00:19 -0600

    Addition of the main pages/index of the project
    Razor components are created for the following modules: Employees, Departments, Positions, Shifts, Zones and Attendance Control. As well as the components shared between them

---
+++ Assistance/Services/MessageService.cs
@@ -0,0 +1,16 @@
+﻿
+
+using static Assistance.Components.Layout.Noification;
+
+namespace Assistance.Services
+{
+    public class MessageService
+    {
+        public event Action<string, NotificationLevel>? OnShow;
+
+        public void ShowNotification(string Message, NotificationLevel NotificationLevel)
+        {
+            OnShow?.Invoke(Message, NotificationLevel);
+        }
+    }
+}
